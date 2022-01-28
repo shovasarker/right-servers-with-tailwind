ad9edeb684d742f61ef867b09ea8f28d5351ad50
ShovaSarker <shovasarker01@gmail.com>
2022-01-29 02:26:37 +0600

    Navbar added in the Header Section

---
+++ dist/js/menu.js
@@ -0,0 +1,28 @@
+//Referencing dom elements
+const menuBtn = document.querySelector('#menu-btn')
+const mobileMenu = document.querySelector('#mobile-menu')
+
+const ourSolutionLink = document.querySelector('#our-solution-link')
+const ourSolutionDropdown = document.querySelector('#our-solution-dropdown')
+const ourSolutionArrow = document.querySelector('#our-solution-arrow')
+
+const fullyManagedLink = document.querySelector('#fully-managed-link')
+const fullyManagedDropdown = document.querySelector('#fully-managed-dropdown')
+const fullyManagedArrow = document.querySelector('#fully-managed-arrow')
+
+//Mobile Menu btn's Click event listener
+menuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('!top-100p')
+})
+
+// Dropdown menu click event listener
+
+ourSolutionLink.addEventListener('click', () => {
+  ourSolutionDropdown.classList.toggle('hidden')
+  ourSolutionArrow.classList.toggle('rotate-180')
+})
+
+fullyManagedLink.addEventListener('click', () => {
+  fullyManagedDropdown.classList.toggle('hidden')
+  fullyManagedArrow.classList.toggle('rotate-180')
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
 module.exports = {
-  content: ['./*.html'],
+  content: ['./*.html', './dist/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        paste: '#44D795',
+        btnBlue: '#2A5CF0',
+        footerBg: '#100C42',
+        gStart: '#01047D',
+        gEnd: '#003ADB',
+      },
+      container: {
+        center: true,
+      },
+      fontSize: {
+        tiny: '15px',
+      },
+      spacing: {
+        '0p': '0%',
+        '10p': '10%',
+        '20p': '20%',
+        '25p': '25%',
+        '50p': '50%',
+        '75p': '75%',
+        '90p': '90%',
+        '100p': '100%',
+        '310p': '310%',
+      },
+    },
   },
   plugins: [],
 }
